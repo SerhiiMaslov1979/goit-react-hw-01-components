d3d722962c2e2faf16a66cc6f8becf2c032d27dc
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-04-16 12:43:56 +0300

    styles friends

--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -5,11 +5,11 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
+    <span className={css.status} style={{ background: (isOnline? "green" : "red") }}></span>
       <img className={css.avatar} src={avatar} alt={name} />
       <span className={css.name}>{name}</span>
 
       
-    <span className={css.status} style={{ background: (isOnline? "green" : "red") }}></span>
     </li>
   );
 };
